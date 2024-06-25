800b6dcc5f2455ec79180037a6a65df88ba1a332
KAVAN THOBHANI <kavanthobhani@KAVANs-MacBook-Air.local>
2024-06-25 23:03:23 +0530

    initial commit

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="bg-pink-50 top-0 w-full flex px-10 py-4 lg:px-20 lg:py-6 z-10 ">
+            <div style={{fontFamily:"var(--font-logo)"}} className="title"><h1 className={"text-5xl font-bold"}>98 Baker's</h1></div>
+        </nav>
+    );
+}
+
+export default Navbar;
